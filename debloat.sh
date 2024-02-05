169a5e1e1e206d346d9d3d617fd851f3d0836121
ar0177417 <ar0177417@gmail.com>
2024-02-05 20:39:03 +0500

    Add debloat.sh

---
+++ debloat.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+mount -o rw,remount /system_root
+cd /system_root/system/product/overlay
+rm -rf *infinix* *sony* *alldocube* *asus* *blackview* *bq-jeice* *duoqin* *fairphone* *htc* *huawei* *lenovo* *lg* *mbi* *meizu* *spen* *moto* *nokia* *nubia* *oneplus* *onn* *oppo* *oukitel* *razer* *realme* *samsung* *sharp* *teclast* *tecno* *teracube* *umidigi* *unihertz* *vivo* *vsmart* *xiaomi*
+cd /system_root/system/system_ext/apex/
+ls | awk '/vndk\.v[0-9][0-9]/ && !/vndk\.v31/' | xargs rm
+rm -rf com.android.vndk.current/lib*
+mount -o ro,remount /system_root
